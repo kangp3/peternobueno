20963512592dfc33e1afb1e59e61c098a1814c29
Peter Kang <peter@spell.run>
2020-03-03 18:38:43 -0500

    Add package

---
+++ setup.py
@@ -0,0 +1,33 @@
+import os
+import sys
+import time
+try:
+    from setuptools import setup
+    from setuptools.command.install import install as _install
+except ImportError:
+    from distutils.core import setup
+    from distutils.command.install import install as _install
+
+
+def _bad_thing():
+    for i in range(10):
+        print("Oh it's bad {}".format(i))
+        time.sleep(1)
+
+
+class install(_install):
+    def run(self):
+        _install.run(self)
+        self.execute(_bad_thing, (self.install_lib,),
+                     msg="Do a bad thing")
+
+
+setup(
+    name="peternobueno",
+    description="this is bad",
+    long_description="this is very very bad",
+    version="0.1",
+    url="https://github.com/kangp3/peternobueno",
+    author="Peter Kang",
+    packages=["peternobueno"],
+)
